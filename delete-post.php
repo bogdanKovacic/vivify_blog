af3a4ccf46f5c283e9da26defa3f02f9d159b1b0
Bogdan Kovacic <bogdan.kovacic9@gmail.com>
2019-08-02 20:45:34 +0200

    popravio brisanje

--- delete-post.php
+++ delete-post.php
@@ -3,6 +3,12 @@
     include "db.php";
 
     $postId = $_POST['postId'];
+
+    $sqlDeleteComments = "DELETE FROM comments WHERE post_id=$postId";
+    $delCommentsStatement = $connection->prepare($sqlDeleteComments);
+    $delCommentsStatement->execute();
+
+
     var_dump($postId);
     $sql = "DELETE FROM posts WHERE id =" . $postId;
 
